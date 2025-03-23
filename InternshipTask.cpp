cfb4be0307cb61b25bd46647efe13877de03022b
KokoDaTako <kunguzov@gmail.com>
2025-03-24 00:37:28 +0200

    [Added] A simple API
    Added a simple API

--- InternshipTask.cpp
+++ InternshipTask.cpp
@@ -1,38 +1,132 @@
 #include <iostream>
 #include <vector>
 
-
+#include "Headers/json.hpp"
+#include "Headers/httplib.h"
 #include "Headers/Flight.h"
 #include "Headers/Route.h"
 
-using namespace std;
+using namespace httplib;
 
-
-
-
+using json = nlohmann::json;
 
 int main()
 {
-	vector<Flight> flights;
+	/*vector<Flight> flights;
 
 	flights.push_back(Flight("SOF", "LON", 20));
 	flights.push_back(Flight("FRA", "LON", 30));
 	flights.push_back(Flight("SOF", "FRA", 10));
 	flights.push_back(Flight("LON", "FRA", 50));
-	
 
-	vector<Route> routeList = Route::PossibleRoutes(flights, "SOF", "LON");
 
-	if (routeList.empty()) cout << "No available routes.";
+	vector<Route> routeList = Route::PossibleRoutes(flights, "SOF", "FRA", 2);
+
+	if (routeList.empty()) std::cout << "No available routes.";
 	else
 	{
 		for (Route rt : routeList)
 		{
 			for (string str : rt.GetRouteStops())
 			{
-				cout << str << " , ";
+				std::cout << str << " , ";
 			}
-			cout << rt.GetRoutePrice() << endl;
+			std::cout << rt.GetRoutePrice() << endl;
 		}
-	}
+	}*/
+
+
+
+	Server svr;
+
+	svr.Post("/routes", [](const Request& req, Response& res)
+		{
+			vector<Flight> flights;
+
+			flights.push_back(Flight("SOF", "LON", 20));
+			flights.push_back(Flight("FRA", "LON", 30));
+			flights.push_back(Flight("SOF", "FRA", 10));
+			flights.push_back(Flight("LON", "FRA", 50));
+
+			json requestBody = json::parse(req.body);
+			
+			vector<Route> routeList;
+			unsigned int maxFlights = UINT_MAX - 1;
+			string origin, destination;
+
+
+			//Checks
+			{
+				//Origin
+				if (!requestBody.contains("origin"))
+				{
+					res.set_content("{\"message\" : \"Missing origin input!\"}", "application/json");
+					return;
+				}
+				if (requestBody["origin"].type() != json::value_t::string)
+				{
+					res.set_content("{\"message\" : \"The input for origin is not in the correct format!\"}", "application/json");
+					return;
+				}
+				origin = requestBody["origin"];
+
+				//Destination
+				if (!requestBody.contains("destination"))
+				{
+					res.set_content("{\"message\" : \"Missing destination input!\"}", "application/json");
+					return;
+				}
+				if (requestBody["destination"].type() != json::value_t::string)
+				{
+					res.set_content("{\"message\" : \"The input for destination is not in the correct format!\"}", "application/json");
+					return;
+				}
+				destination = requestBody["destination"];
+
+				//maxFlights
+				if (requestBody.contains("maxFlights"))
+				{
+
+					if (requestBody["maxFlights"].type() != json::value_t::number_unsigned)
+					{
+						res.set_content("{\"message\" : \"The input for maxFlights is not in the correct format\"}", "application/json");
+						return;
+					}
+					if ((unsigned long long)requestBody["maxFlights"] != (unsigned int)requestBody["maxFlights"])
+					{
+						res.set_content("{\"message\" : \"You have gone over the maximum amount for the maxFlight filter\"}", "application/json");
+						return;
+					}
+					maxFlights = requestBody["maxFlights"];
+				}
+
+				//Correct filters -> create route list
+				routeList = Route::PossibleRoutes(flights, origin, destination, maxFlights);
+
+				//Check if route list is empty
+				if (routeList.empty())
+				{
+					res.set_content("{\"message\" : \"No routes available!\"}", "application/json");
+					return;
+				}
+			}
+			
+
+			json response;
+			for (Route route : routeList)
+			{
+				response.push_back({
+					{"price", route.GetRoutePrice()},
+					{"route", route.GetRouteStops()}
+
+					});
+			}
+
+			res.set_content(response.dump(), "application/json");
+		});
+
+
+	svr.listen("localhost", 5000);
+
+
 }
